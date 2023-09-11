7fb70be5d9279d48df81a005ddbcb3aafbb53bce
medvedevmr <medvedevmr.work@gmail.com>
2023-09-11 19:58:50 +0300

    Bug fixed

--- stats_visualization.py
+++ stats_visualization.py
@@ -24,9 +24,11 @@ def stats_pic():
     # compute circle positions:
     circles = circlify.circlify(
         group_vals, 
-        show_enclosure=False, 
+        show_enclosure=True, 
         target_enclosure=circlify.Circle(x=0, y=0, r=1)
     )
+    
+    
 
     # Create just a figure and only one subplot
     fig, ax = plt.subplots(figsize=(10,10))
@@ -47,10 +49,9 @@ def stats_pic():
     )
     plt.xlim(-lim, lim)
     plt.ylim(-lim, lim)
-    
     colors = ["#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65"]
     # print circles
-    for circle, label, val, color in zip(circles, group_keys, group_vals, colors):
+    for circle, label, color in zip(circles, group_keys, colors):
         x, y, r = circle
         ax.add_patch(plt.Circle((x, y), r, alpha=0.2, linewidth=2,facecolor=color))
         plt.annotate(
@@ -60,13 +61,13 @@ def stats_pic():
             ha='center'
         )
         plt.annotate(
-            val,
+            circle.ex['datum'],
             (x,y-0.01 ) ,
             va='center',
             ha='center'
         )
         plt.annotate(
-            str(round((val/group_sum)*100,2))+'%',
+            str(round((circle.ex['datum']/group_sum)*100,2))+'%',
             (x,y-0.05 ) ,
             va='center',
             ha='center'
@@ -74,4 +75,3 @@ def stats_pic():
     file_name = 'temp_pics/{}-{}-{}.png'.format(date[0],date[1],date[2])
     plt.savefig(file_name)
     return file_name
-stats_pic()
